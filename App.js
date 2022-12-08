c32f5d2705c13c24bd44af7a918b647cd11f04bb
George Kibe <georgekibewambui@gmail.com>
2022-12-08 09:31:56 +0300

    testing animations

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import HomeScreen from './src/screens/HomeScreen'
 import "react-native-gesture-handler"
 
+//continue from 2:19:00
+
 const App = () => {
   return (
     <View style={styles.pageContainer}>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,56 +1,62 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Pressable , useWindowDimensions} from 'react-native'
 import React from 'react'
 import TinderCard from '../components/TinderCard'
 import Animated, { 
   useSharedValue, 
   useAnimatedStyle, 
-  withSpring,
+  useDerivedValue,
+  interpolate,
   useAnimatedGestureHandler } from 'react-native-reanimated'
 import { PanGestureHandler } from 'react-native-gesture-handler'
 
 import users from "../assets/data/users"
 
 const HomeScreen = () => {
-  const sharedValue = useSharedValue(1);
+  const {width:screenWidth} = useWindowDimensions();
+
+  const translateX = useSharedValue(0);            //-width    0     +width
+  const rotate = useDerivedValue(() => interpolate(
+    translateX.value, [0, screenWidth], [0, 60]) +'deg')     //60 deg   0 deg  60deg
+ 
+  const cardStyle = useAnimatedStyle(() => ({
+    transform:[
+      {
+        translateX:translateX.value,
+      },
+      {
+        rotate: rotate.value,
+      }
+    ],
+  }));
+
   const gestureHandler = useAnimatedGestureHandler({
-    onStart: () => {
+    onStart: (_, context) => {
+      context.startX=translateX.value
       console.log("Touch Start");
     },
-    onActive: (event) => {
-      console.log("Touch X: ", event.translationX);
+    onActive: (event, context) => {
+      translateX.value=context.startX + event.translationX
+      //console.log("Touch X: ", event.translationX);
     },
     onEnd: () => {
       console.log("Touch Ended!")
     }
   });
-  const cardStyle = useAnimatedStyle(() => ({
-    transform:[{
-      translateX:sharedValue.value*500 - 250
-    }],
-    opacity:sharedValue.value
-  }));
 
   return (
-    <>
-      <Animated.View style={[StyleSheet.animatedCard, cardStyle]}>
+    <PanGestureHandler onGestureEvent={gestureHandler}>
+      <Animated.View style={[styles.animatedCard, cardStyle]}>
         <TinderCard user={users[1]} />
       </Animated.View>
-      <Pressable onPress={() => (sharedValue.value = withSpring(Math.random()))}>
-        <Text>Change Value</Text>
-      </Pressable>
-    </>
-  
-    
+    </PanGestureHandler>
   )
 }
 
 const styles = StyleSheet.create({
   animatedCard:{
-    height:"100%",
     width:"100%",
     justifyContent:"center",
     alignItems:"center",
-    flex:1
   }
 })
 
